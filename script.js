49b97ddab573bea3873768fadbe10f0765a5e0cb
LukM88 <58972045+LukM88@users.noreply.github.com>
2024-09-19 17:23:47 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleDetails(element) {
+    var details = element.nextElementSibling;
+    if (details.style.display === "block") {
+        details.style.display = "none";
+    } else {
+        details.style.display = "block";
+    }
+}
