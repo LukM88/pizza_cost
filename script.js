2605106055d136d3e486c717215669dd44a94d4c
LukM88 <mrzygod@gmail.com>
2024-09-19 18:10:34 +0200

    fix data and add content to list

--- script.js
+++ script.js
@@ -1,31 +1,26 @@
-// Funkcja do wczytywania pliku data.json
-fetch('data.json')
-    .then(response => response.json())
-    .then(data => {
-        const resultsList = document.getElementById('results-list');
-        
-        // Przeiteruj przez każdy element w pliku JSON i dodaj go do listy wyników
-        data.forEach(item => {
-            // Tworzenie elementów listy
-            const listItem = document.createElement('li');
-            const titleDiv = document.createElement('div');
-            const detailsDiv = document.createElement('div');
-            
-            // Ustawienia dla tytułu i szczegółów
-            titleDiv.classList.add('result-title');
-            titleDiv.textContent = item.title;
-            titleDiv.onclick = function() { toggleDetails(this); };
+const searchInput = document.querySelector('.input')
+const resultsList = document.getElementById('results-list');
+const clearButton = document.getElementById('clear')
 
-            detailsDiv.classList.add('result-details');
-            detailsDiv.textContent = item.details;
+searchInput.addEventListener("input", (e) => {
+    let value = e.target.value
 
-            // Dodanie elementów do listy
-            listItem.appendChild(titleDiv);
-            listItem.appendChild(detailsDiv);
-            resultsList.appendChild(listItem);
-        });
-    })
-    .catch(error => console.error('Błąd przy wczytywaniu danych:', error));
+    if (value && value.trim().length > 0){
+        // Funkcja do wczytywania pliku data.json
+        fetch('data.json')
+        .then(response => response.json())
+        .then(data => {
+            setList(data.filter(record => {
+                return record.street.includes(value)
+            }))
+        })
+        .catch(error => console.error('Błąd przy wczytywaniu danych:', error));
+    }
+})
+
+clearButton.addEventListener("click", () => {
+    clearList()
+})
 
 // Funkcja do rozwijania szczegółów
 function toggleDetails(element) {
@@ -35,4 +30,44 @@ function toggleDetails(element) {
     } else {
         details.style.display = "block";
     }
+}
+
+function clearList(){
+    while (resultsList.firstChild){
+        resultsList.removeChild(resultsList.firstChild)
+    }
+}
+
+function setList(results){
+    clearList()
+    // Przeiteruj przez każdy element w pliku JSON i dodaj go do listy wyników
+    results.forEach(item => {
+        // Tworzenie elementów listy
+        let listItem = document.createElement('li');
+        let titleDiv = document.createElement('div');
+        let detailsDiv = document.createElement('div');
+        let cityLabel = document.createElement('div')
+        let streetLabel = document.createElement('div')
+        let costLabel = document.createElement('div')
+        let regionLabel = document.createElement('div')
+        
+        // Ustawienia dla tytułu i szczegółów
+        titleDiv.classList.add('result-title');
+        titleDiv.textContent = item.street;
+        titleDiv.onclick = function() { toggleDetails(this); };
+        detailsDiv.classList.add('result-details');
+        
+        costLabel.textContent = "Koszt dostawy:".concat(' ', item.cost, 'zł')
+        cityLabel.textContent = "Miejscowość:".concat(' ', item.city)
+        streetLabel.textContent = "Ulica:".concat(' ', item.street)
+        regionLabel.textContent = "Region dostawy:".concat(' ', item.region)
+        // Dodanie elementów do listy
+        listItem.appendChild(titleDiv);
+        listItem.appendChild(detailsDiv);
+        detailsDiv.appendChild(cityLabel)
+        detailsDiv.appendChild(regionLabel)
+        detailsDiv.appendChild(streetLabel)
+        detailsDiv.appendChild(costLabel)
+        resultsList.appendChild(listItem);
+    });
 }
